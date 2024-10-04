90c06160d9bbcc5bfb882648d858859ab8375f15
pedro-santos-aluno <santos.1850.aluno.senac@gmail.com>
2024-10-04 10:15:07 -0300

    04/101015

---
+++ curriculo_html_css/script.js
@@ -0,0 +1,7 @@
+//========================= VARIÁVEIS DO MENU ===================================
+var icone_menu = document.querySelector('.celular');
+var lista_menu = document.querySelector('.opcoes');
+//monitora o icone menu, se esta tendo um click
+icone_menu.addEventListener('click',function(){
+
+});
